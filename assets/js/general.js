35771a7609b6c370712e7066786badc147461f6a
jquintinita <joseph.quintinita@gmail.com>
2022-10-03 00:35:08 +0800

    responsive

--- assets/js/general.js
+++ assets/js/general.js
@@ -12,4 +12,41 @@ $('.testi-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     centerMode: true,
+    responsive: [
+        {
+          breakpoint: 980,
+          settings: {
+            arrows: true,
+            centerMode: true,
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: true,
+            centerMode: false,
+            slidesToShow: 1
+          }
+        }
+      ]
+});
+
+
+$(function() {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('nav .menu-nav a, .footer-link a').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000); // The number here represents the speed of the scroll in milliseconds
+          return false;
+        }
+      }
+    });
   });
+  
