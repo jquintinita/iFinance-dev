418ee88e4434a43fa30c2e4528b209a27688363d
jquintinita <joseph.quintinita@gmail.com>
2022-10-04 21:04:35 +0800

    updated oct 4

--- assets/js/general.js
+++ assets/js/general.js
@@ -6,6 +6,30 @@ slider.oninput = function() {
   output.innerHTML = this.value ;
 }
 
+let aspectRatioGroup = document.querySelector('.form-amount-range .segmentedControl');
+let radios = aspectRatioGroup.querySelectorAll('input');
+let i = 1;
+
+// set CSS Var to number of radios we have
+aspectRatioGroup.style.setProperty('--options',radios.length);
+
+// loop through radio elements
+radios.forEach((input)=>{
+	// store position as data attribute
+	input.setAttribute('data-pos',i);
+	
+	// add click handler to change position
+	input.addEventListener('click',(e)=>{
+		aspectRatioGroup.style.setProperty('--options-active',e.target.getAttribute('data-pos'));
+	});
+	
+	// increment counter
+	i++;
+});
+
+// add class to enable the sliding pill animation, otherwise it uses a fallback
+aspectRatioGroup.classList.add('useSlidingAnimation');
+
 
 $('.testi-slider').slick({
     infinite: true,
